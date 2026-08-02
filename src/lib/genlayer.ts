ca04fd2bc5c73e8857f9d022f02c9cca3c434290
nikvn89 <nikvn89@gmail.com>
2026-08-02 09:58:13 +0700

    Fix SDK chain import (studionet instead of simulator)

--- src/lib/genlayer.ts
+++ src/lib/genlayer.ts
@@ -2,10 +2,10 @@ import { createClient } from "genlayer-js";
 
 // Ensure you are pointing to the correct chain (localnet/simulator/studionet)
 // For hackathon purposes, we use the default exported configurations.
-import { simulator } from "genlayer-js/chains";
+import { studionet } from "genlayer-js/chains";
 
 export const CONTRACT_ADDRESS = "0x0726F885f4bb75BAb9d7b8b8cF3941525F6FA9CA";
 
 export const client = createClient({
-  chain: simulator,
+  chain: studionet,
 });
